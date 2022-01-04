e94a5526f8faeadbd3dd1d358c7d79ab80df187d
Alexander Voigt <ahgvoigt@gmail.com>
2022-01-04 12:12:19 +0100

    improve documentation

--- src/Cl.jl
+++ src/Cl.jl
@@ -141,11 +141,11 @@ end
     cl(n::Int64, x::Float64)::Float64
 
 Returns the value of the Clausen function ``\\operatorname{Cl}_n(x)``
-for integer `n > 1` and a real angle `x` of type `Float64`.  This
+for integer ``n > 1`` and a real angle ``x`` of type `Float64`.  This
 function is defined as
 
 ```math
-\\operatorname{Cl}_n(x) &= \\Im[\\operatorname{Li}_n(e^{ix})] = \\sum_{k=1}^\\infty \\frac{\\sin(kx)}{k^n}, \\qquad n~\\text{even}, \\
+\\operatorname{Cl}_n(x) &= \\Im[\\operatorname{Li}_n(e^{ix})] = \\sum_{k=1}^\\infty \\frac{\\sin(kx)}{k^n}, \\qquad n~\\text{even}, \\\\
 \\operatorname{Cl}_n(x) &= \\Re[\\operatorname{Li}_n(e^{ix})] = \\sum_{k=1}^\\infty \\frac{\\cos(kx)}{k^n}, \\qquad n~\\text{odd}
 ```
 

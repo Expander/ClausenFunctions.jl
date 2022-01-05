2b00dc2a38e458fcb5ae47d86ca69de754a831f2
Alexander Voigt <ahgvoigt@gmail.com>
2022-01-05 17:28:16 +0100

    split function for range reduction for even/odd n

--- src/range_reduction.jl
+++ src/range_reduction.jl
@@ -1,5 +1,24 @@
-# returns (x, sign) with x in [0,pi]
-function range_reduce(n::Integer, x::Float64)
+# returns x in [0,pi] for odd n
+function range_reduce_odd(x::Float64)
+    if x < zero(x)
+        x = -x
+    end
+
+    if x >= 2.0*pi
+        x = mod2pi(x)
+    end
+
+    if x > pi
+        p0 = 6.28125
+        p1 = 0.0019353071795864769253
+        x = (p0 - x) + p1
+    end
+
+    x
+end
+
+# returns x in [0,pi] for even n
+function range_reduce_even(x::Float64)
     sgn = one(x)
 
     if x < zero(x)
@@ -18,9 +37,14 @@ function range_reduce(n::Integer, x::Float64)
         sgn = -sgn
     end
 
+    (x, sgn)
+end
+
+# returns (x, sign) with x in [0,pi]
+function range_reduce(n::Integer, x::Float64)
     if iseven(n)
-        (x, sgn)
+        range_reduce_even(x)
     else
-        (x, one(x))
+        (range_reduce_odd(x), one(x))
     end
 end

962f8f6c67a182220eb2e0bda69d1da85ff0f2ab
Alexander Voigt <ahgvoigt@gmail.com>
2022-12-29 14:32:40 +0100

    merge if statements
    Note: If pi is passed, pi is converted to a float. In this case
    the condition
    
      3.141592653589793 < pi
    
    is always true.

--- test/Cl2.jl
+++ test/Cl2.jl
@@ -15,6 +15,8 @@
         @test ClausenFunctions.cl2(Float32(x)) ≈ Float32(expected) atol=30*eps(Float32) rtol=30*eps(Float32)
     end
 
+    @test ClausenFunctions.cl2(0.0) == 0.0
+    @test ClausenFunctions.cl2(pi) == 0.0
     @test ClausenFunctions.cl2(pi/2) ≈ 0.915965594177219015054603514932384110 rtol=1e-14
     @test ClausenFunctions.cl2(1//2) ≈ 0.84831187770367927 rtol=1e-14
 end

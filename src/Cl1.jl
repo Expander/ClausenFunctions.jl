f1fb021417d29f4ecbf80636f061e6adeae63278
Alexander Voigt <Expander@users.noreply.github.com>
2023-07-25 18:13:23 +0200

    Add implementation of cl1 for complex argument (#5)
    and arbitrary precision

--- src/Cl1.jl
+++ src/Cl1.jl
@@ -1,14 +1,15 @@
 """
-    cl1(x::Real)::Real
+    cl1(z)
 
-Returns the value of the Clausen function ``\\operatorname{Cl}_1(x)``
-for a real angle ``x`` of type `Real`.  This function is defined as
+Returns the value of the Clausen function ``\\operatorname{Cl}_1(z)``
+for an argument ``z`` of type `Real` or `Complex`.  This function is
+defined as
 
 ```math
-\\operatorname{Cl}_1(x) = \\Re[\\operatorname{Li}_1(e^{ix})] = \\Re[-\\log(1 - e^{ix})]
+\\operatorname{Cl}_1(z) = -\\frac{1}{2}\\left[\\log(1 - e^{iz}) + \\log(1 - e^{-iz})\\right]
 ```
 
-Note: ``\\operatorname{Cl}_1(x)`` is not defined for ``x=2k\\pi`` with
+Note: ``\\operatorname{Cl}_1(z)`` is not defined for ``z=2k\\pi`` with
 ``k\\in\\mathbb{Z}``.
 
 Author: Alexander Voigt
@@ -19,15 +20,18 @@ License: MIT
 ```jldoctest; setup = :(using ClausenFunctions), output = false
 julia> cl1(1.0)
 0.04201950582536895
+
+julia> cl1(1.0 + 1.0im)
+-0.3479608285425303 - 0.7021088550913619im
 ```
 """
-cl1(x::Real) = _cl1(float(x))
+cl1
 
-_cl1(x::Float16) = oftype(x, _cl1(Float32(x)))
+cl1(x::Float16) = oftype(x, cl1(Float32(x)))
 
-_cl1(x::Float32) = oftype(x, _cl1(Float64(x)))
+cl1(x::Float32) = oftype(x, cl1(Float64(x)))
 
-function _cl1(x::Float64)::Float64
+function cl1(x::Float64)::Float64
     x = range_reduce_odd(x)
 
     if x == zero(x)
@@ -35,4 +39,13 @@ function _cl1(x::Float64)::Float64
     end
 
     -log(2*sin(one(x)/2*x))
+end
+
+function cl1(x::Real)
+    -log(hypot(one(x) - cos(x), sin(x)))
+end
+
+function cl1(z::Complex)
+    eiz = exp(im*z)
+    -(log(one(z) - eiz) + log(one(z) - inv(eiz)))/2
 end
--- test/Cl1.jl
+++ test/Cl1.jl
@@ -13,9 +13,37 @@
 
         @test ClausenFunctions.cl1(Float16(x)) ≈ Float16(expected) atol=10*eps(Float16) rtol=30*eps(Float16)
         @test ClausenFunctions.cl1(Float32(x)) ≈ Float32(expected) atol=10*eps(Float32) rtol=30*eps(Float32)
+
+        @test ClausenFunctions.cl1(x) ≈ real(ClausenFunctions.cl1(Complex(x))) rtol=1e-13
     end
 
     @test ClausenFunctions.cl1(1//2) ≈ 0.70358563513784466 rtol=1e-14
-    @test ClausenFunctions.cl1(1) == ClausenFunctions.cl1(1.0)
+    @test ClausenFunctions.cl1(1) ≈ ClausenFunctions.cl1(1.0) rtol=1e-14
     @test ClausenFunctions.cl1(0.0) == Inf
+
+    # test complex Clausen function cl1
+    @test ClausenFunctions.cl1(Complex(1/2)) ≈ 0.70358563513784466 rtol=1e-14
+    @test ClausenFunctions.cl1(Complex(1/2, 1)) ≈ -0.14296382186432422 - 1.06599896593689653im rtol=1e-14
+    @test ClausenFunctions.cl1(Complex(1/2, -1)) ≈ -0.14296382186432422 + 1.06599896593689653im rtol=1e-14
+    @test ClausenFunctions.cl1(Complex(1, 0)) ≈ 0.042019505825368962 rtol=1e-14
+    @test ClausenFunctions.cl1(Complex(1, 1)) ≈ -0.34796082854253047 - 0.70210885509136190im rtol=1e-14
+    @test ClausenFunctions.cl1(Complex(2, 0)) ≈ -0.52054343429085363 rtol=1e-14
+    @test ClausenFunctions.cl1(Complex(1.0, 0.0)) ≈ 0.042019505825368962 rtol=1e-14
+    @test ClausenFunctions.cl1(Complex(1.0, 1.0)) ≈ -0.34796082854253047 - 0.70210885509136190im rtol=1e-14
+    @test ClausenFunctions.cl1(Complex(2.0, 0.0)) ≈ -0.52054343429085363 rtol=1e-14
+    @test ClausenFunctions.cl1(Complex(2.0, 1.0)) ≈ -0.68284871442091651 - 0.28844676165196074im rtol=1e-14
+    @test ClausenFunctions.cl1(Complex(2.0, -1.0)) ≈ -0.68284871442091651 + 0.28844676165196074im rtol=1e-14
+    @test imag(ClausenFunctions.cl1(Complex(2.0, eps(Float64)))) < 0.0
+    @test imag(ClausenFunctions.cl1(Complex(2.0, -eps(Float64)))) > 0.0
+    @test ClausenFunctions.cl1(Complex(2.0, -eps(Float64))) ≈ -0.52054343429085363 rtol=1e-14
+    @test ClausenFunctions.cl1(Complex(0)) == Inf
+    @test ClausenFunctions.cl1(Complex(0.0)) == Inf
+
+    # test BigFloat
+    setprecision(BigFloat, 100) do
+        @test isapprox(ClausenFunctions.cl1(BigFloat("1")), BigFloat("0.042019505825368961725798384037902037124538920557034"); rtol=10*eps(BigFloat))
+        @test isapprox(ClausenFunctions.cl1(BigFloat("0.5") + BigFloat("1")*im), BigFloat("-0.14296382186432422499606514633517958638642188617714") + BigFloat("-1.0659989659368965336455070335833947446477603110289")*im; rtol=10*eps(BigFloat))
+        @test isapprox(ClausenFunctions.cl1(BigFloat("2") + BigFloat("-1")*im), BigFloat("-0.68284871442091650950302220327572227276874425568964") + BigFloat("0.28844676165196073566657598635590990207739789086077")*im; rtol=10*eps(BigFloat))
+        @test isapprox(ClausenFunctions.cl1(BigFloat("0")), BigFloat("Inf"); rtol=10*eps(BigFloat))
+    end
 end

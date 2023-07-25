7215efb117d653333d4f4172f2735a2f5a21e67e
Alexander Voigt <ahgvoigt@gmail.com>
2023-07-25 17:56:42 +0200

    adding tests for BigFloat

--- test/Cl1.jl
+++ test/Cl1.jl
@@ -38,4 +38,12 @@
     @test ClausenFunctions.cl1(Complex(2.0, -eps(Float64))) ≈ -0.52054343429085363 rtol=1e-14
     @test ClausenFunctions.cl1(Complex(0)) == Inf
     @test ClausenFunctions.cl1(Complex(0.0)) == Inf
+
+    # test BigFloat
+    setprecision(BigFloat, 100) do
+        @test isapprox(ClausenFunctions.cl1(BigFloat("1")), BigFloat("0.042019505825368961725798384037902037124538920557034"); rtol=10*eps(BigFloat))
+        @test isapprox(ClausenFunctions.cl1(BigFloat("0.5") + BigFloat("1")*im), BigFloat("-0.14296382186432422499606514633517958638642188617714") + BigFloat("-1.0659989659368965336455070335833947446477603110289")*im; rtol=10*eps(BigFloat))
+        @test isapprox(ClausenFunctions.cl1(BigFloat("2") + BigFloat("-1")*im), BigFloat("-0.68284871442091650950302220327572227276874425568964") + BigFloat("0.28844676165196073566657598635590990207739789086077")*im; rtol=10*eps(BigFloat))
+        @test isapprox(ClausenFunctions.cl1(BigFloat("0")), BigFloat("Inf"); rtol=10*eps(BigFloat))
+    end
 end

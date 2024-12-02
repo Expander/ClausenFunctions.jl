667c16843cbf81f6e9c7cb527073d2a4f95f4788
Alexander Voigt <Expander@users.noreply.github.com>
2024-12-02 06:46:21 +0100

    Feature: Add Missing overloads (#10)

---
+++ test/Missing.jl
@@ -0,0 +1,13 @@
+@testset "Missing" begin
+    @test ismissing(ClausenFunctions.cl1(missing))
+    @test ismissing(ClausenFunctions.cl2(missing))
+    @test ismissing(ClausenFunctions.cl3(missing))
+    @test ismissing(ClausenFunctions.cl4(missing))
+    @test ismissing(ClausenFunctions.cl5(missing))
+    @test ismissing(ClausenFunctions.cl6(missing))
+
+    for n in -16:16
+        @test ismissing(ClausenFunctions.cl(n, missing))
+        @test ismissing(ClausenFunctions.sl(n, missing))
+    end
+end
